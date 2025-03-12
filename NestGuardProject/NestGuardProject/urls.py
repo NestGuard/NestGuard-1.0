3997add4dcc7f14cdd7d27b64d64b99643907235
Guilherme-Augusto-06 <ga375464@gmail.com>
2025-03-12 14:55:58 -0300

    fix: comment out admin URL path in urls.py

--- NestGuardProject/NestGuardProject/urls.py
+++ NestGuardProject/NestGuardProject/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('AppNestGuard.urls')),         # Inclui as urls do app blog
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
